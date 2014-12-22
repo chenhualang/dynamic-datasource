52d33915b8963c66fb8fbb99951d03579ba4bd57
王金兆 <737597978@qq.com>
2014-12-22 18:23:17 +0800

    创建项目

---
+++ src/main/java/com/wangjinzhao/dynamic/datasource/domain/Tenant.java
@@ -0,0 +1,140 @@
+package com.wangjinzhao.dynamic.datasource.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by 737597978 on 2014/12/22.
+ */
+public class Tenant  implements Serializable{
+
+    private Long tenantId;
+
+    private String name;
+
+    private String description;
+
+    private Long orgId;
+
+    private Date effectiveTime;
+
+    private Date expiryTime;
+
+    private Long creator;
+
+    private Date dateCreated;
+
+    private Long changedBy;
+
+    private Date dateChanged;
+
+    private Short status;
+
+    private Short type;
+
+    private String domainName;
+
+    public Long getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(Long orgId) {
+        this.orgId = orgId;
+    }
+
+    public Date getEffectiveTime() {
+        return effectiveTime;
+    }
+
+    public void setEffectiveTime(Date effectiveTime) {
+        this.effectiveTime = effectiveTime;
+    }
+
+    public Date getExpiryTime() {
+        return expiryTime;
+    }
+
+    public void setExpiryTime(Date expiryTime) {
+        this.expiryTime = expiryTime;
+    }
+
+    public Long getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Long creator) {
+        this.creator = creator;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Long getChangedBy() {
+        return changedBy;
+    }
+
+    public void setChangedBy(Long changedBy) {
+        this.changedBy = changedBy;
+    }
+
+    public Date getDateChanged() {
+        return dateChanged;
+    }
+
+    public void setDateChanged(Date dateChanged) {
+        this.dateChanged = dateChanged;
+    }
+
+    public Short getStatus() {
+        return status;
+    }
+
+    public void setStatus(Short status) {
+        this.status = status;
+    }
+
+    public Short getType() {
+        return type;
+    }
+
+    public void setType(Short type) {
+        this.type = type;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+}
